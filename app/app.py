c52cc1d6737156393af070d1dc669b4210e981a8
Hetzer123 <146844242+Hetzer123@users.noreply.github.com>
2023-10-22 20:25:51 +0800

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from joblib import load
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    request_type_str = request.method
+    if request_type_str == 'GET':
+        return render_template('index.html', href2='')
+    else:
+        mysalary = request.form['age']
+        mygender = request.form['gender']
+        model = load('app/watch-recommender.joblib')
+        np_arr = np.array([mysalary, mygender])
+        predictions = model.predict([np_arr])  
+        predictions_to_str = str(predictions)
+        #return predictions_to_str
+        return render_template('index.html', href2='The suitable Watch for you (salary:'+str(mysalary)+' ,gender:'+str(mygender)+' ,) is:'+predictions_to_str)
+
+
+
